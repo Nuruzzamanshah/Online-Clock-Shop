a67a822955f3d6dfa23b42a33694ab484c0de7ff
Md Nuruzzaman <mnueuzzaman@gmail.com>
2022-03-26 02:43:49 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Online <span className="header-color">Phone</span> Shop</h1>
+            <h2>Choose a new phone</h2>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {name, picture, company,price, eyeColor} = props.product;
+    return (
+        <div className="product">
+            <img src={picture} alt="" />
+            <div className="product-info">
+                <p className="product-name">Name: {name}</p>
+                <p>Price: ${price}</p>
+                <p>Company: {company}</p>
+                <p>Color: {eyeColor}</p>
+            </div>
+            <button className="btn-card">
+                <p>Add To Card</p>
+            </button>
+        </div>
+    );
+};
+
+export default Product;
