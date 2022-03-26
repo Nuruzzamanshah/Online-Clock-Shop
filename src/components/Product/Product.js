6b75b68096ec47bd1c1e9a8113e1ba7121015460
Md Nuruzzaman <mnueuzzaman@gmail.com>
2022-03-26 11:14:29 +0600

    first commit

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,6 +3,7 @@ import './Product.css'
 
 const Product = (props) => {
     const {name, picture, company,price, eyeColor} = props.product;
+    
     return (
         <div className="product">
             <img src={picture} alt="" />
@@ -12,7 +13,7 @@ const Product = (props) => {
                 <p>Company: {company}</p>
                 <p>Color: {eyeColor}</p>
             </div>
-            <button className="btn-card">
+            <button onClick={ () =>props.handleClick(props.product)} className="btn-card">
                 <p>Add To Card</p>
             </button>
         </div>
