a88b8c7f0634a5b6afe1df3f91a694a00ef8c670
Md Nuruzzaman <mnueuzzaman@gmail.com>
2022-03-26 19:30:05 +0600

    css added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <h1>Online <span className="header-color">Phone</span> Shop</h1>
-            <h2>Choose a new phone</h2>
+            <h1>Online <span className="header-color">Clock</span> Shop</h1>
+            <h2>Choose a New Clock</h2>
         </div>
     );
 };
